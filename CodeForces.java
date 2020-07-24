7028ef400faf065e3f8924c6f0f2c2099deb628d
Khadjiev <48748846+Khadjiev@users.noreply.github.com>
2020-07-24 15:51:32 +0300

    Add CodeForces java extension template.
    A good Intellij Idea Java Extention template I created for developers to practice on CodeForces Codforces.

---
+++ CodeForces.java
@@ -0,0 +1,46 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+#parse("File Header.java")
+public class ${NAME} {
+
+    public static void main(String[] args) {
+        new ${NAME}().init();
+    }
+
+    /**
+     * Initializing the solution by taking input values from the console
+     */
+    public void init() {
+        // Reading input from the console
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // The number of test cases
+        int tc;
+        // Input values per each case, separated by space
+        String[] inputValues;
+
+        try {
+            tc = Integer.parseInt(reader.readLine());
+            while (tc-- != 0) {
+                inputValues = reader.readLine().split(" ");
+                solveProblem(inputValues);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * Main solution method
+     * @param inVals input values separated by space
+     */
+    private void solveProblem(String[] inVals) {
+        for (String val : inVals) {
+            // walk with the solution
+        }
+    }
+}
